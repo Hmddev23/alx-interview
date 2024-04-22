6df3425475b0eaf67849308428da65b59b24a7e4
Hmddev23 <hamid.maira709@gmail.com>
2024-04-22 14:48:58 -0700

    pascal triangle 1

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Generate Pascal's triangle up to the nth row.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+    """
+    return 0
