09ceb8b1b3c4b8c5275269559bb63a7b4d99ffb6
Hmddev23 <hamid.maira709@gmail.com>
2024-04-25 15:52:34 -0700

    pascal fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,4 +8,15 @@ def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the nth row.
     """
-    return 0
+    if n <= 0:
+        return []
+    pascal = [[1]]
+    for col in range(1, n):
+        row = [1]
+        for j in range(1, col):
+            value = pascal[col - 1][j - 1] + pascal[col - 1][j]
+            row.append(value)
+        row.append(1)
+        pascal.append(row)
+
+    return pascal
