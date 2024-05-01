6ce704d49aa4e3aead256f67b00c4f98b95c2630
Hmddev23 <hamid.maira709@gmail.com>
2024-05-01 22:46:26 +0000

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+determine if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    determine if all the boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list of lists where each inner
+        list represents a box, and the indices of the outer list
+        represent the box numbers.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    nbr_boxes = len(boxes)
+    key_pairs = [0]
+    ctr = 0
+    pointer = 0
+
+    while pointer < len(key_pairs):
+        key = key_pairs[pointer]
+        for key in boxes[key]:
+            if 0 < key < nbr_boxes and key not in key_pairs:
+                key_pairs.append(key)
+                ctr += 1
+        pointer += 1
+
+    return ctr == nbr_boxes - 1
